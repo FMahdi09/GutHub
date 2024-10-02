fce92c34bd83de29f32a7ba244603ea07565c919
Fabian Mahdi <Fabian@Mahdi.at>
2024-10-02 14:53:19 +0800

    added models for users + roles

---
+++ Backend/src/main/java/Guthub/Backend/Models/Role.java
@@ -0,0 +1,36 @@
+package Guthub.Backend.Models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role
+{
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(
+            unique = true,
+            nullable = false
+    )
+    private String name;
+
+    public Role(String name)
+    {
+        this.name = name;
+    }
+
+    public GrantedAuthority getAuthority()
+    {
+        return new SimpleGrantedAuthority("ROLE_" + name);
+    }
+}
