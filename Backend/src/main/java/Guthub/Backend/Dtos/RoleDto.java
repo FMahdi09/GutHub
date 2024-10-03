2c87a4cce5991780c4f7a2b0cdede1e82dd0717d
Fabian Mahdi <Fabian@Mahdi.at>
2024-10-03 10:13:27 +0800

    added all args ctor to dtos

--- Backend/src/main/java/Guthub/Backend/Dtos/RoleDto.java
+++ Backend/src/main/java/Guthub/Backend/Dtos/RoleDto.java
@@ -1,10 +1,12 @@
 package Guthub.Backend.Dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class RoleDto
 {
     private String name;
