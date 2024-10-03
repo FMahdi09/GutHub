3facc24d5ee7f7a1f8c3cc4e3cf8f2e8166e919b
Fabian Mahdi <Fabian@Mahdi.at>
2024-10-03 15:04:35 +0800

    implemented access and refresh token generation

--- Backend/src/main/java/Guthub/Backend/Services/Token/Impl/JwtTokenService.java
+++ Backend/src/main/java/Guthub/Backend/Services/Token/Impl/JwtTokenService.java
@@ -2,34 +2,55 @@ package Guthub.Backend.Services.Token.Impl;
 
 import Guthub.Backend.Configuration.JwtConfiguration;
 import Guthub.Backend.Services.Token.TokenService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
 import java.util.Date;
 
 @Service
 public class JwtTokenService implements TokenService
 {
-    private final String accessTokenSecret;
+    private final Key accessTokenSecret;
 
-    private final String refreshTokenSecret;
+    private final Key refreshTokenSecret;
 
     @Autowired
     public JwtTokenService(JwtConfiguration config)
     {
-        this.accessTokenSecret = config.getAccessTokenSecret();
-        this.refreshTokenSecret = config.getRefreshTokenSecret();
+        this.accessTokenSecret = getSigningKey(config.getAccessTokenSecret());
+        this.refreshTokenSecret = getSigningKey(config.getRefreshTokenSecret());
     }
 
     @Override
     public String generateAccessToken(String subject, Date issuedAt, Date expiresAt)
     {
-        return "";
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiresAt)
+                .signWith(accessTokenSecret, SignatureAlgorithm.HS512)
+                .compact();
     }
 
     @Override
     public String generateRefreshToken(String subject, Date issuedAt, Date expiresAt)
     {
-        return "";
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiresAt)
+                .signWith(refreshTokenSecret, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    private Key getSigningKey(String base64Secret)
+    {
+        byte[] keyBytes = Decoders.BASE64.decode(base64Secret);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
