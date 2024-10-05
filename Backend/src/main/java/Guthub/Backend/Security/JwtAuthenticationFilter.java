8bd02c0a42a355572db01dd2ea39e172377c119e
Fabian Mahdi <Fabian@Mahdi.at>
2024-10-05 11:45:31 +0800

    getServletPath to getRequestUri because servletPath is null inside a filter

--- Backend/src/main/java/Guthub/Backend/Security/JwtAuthenticationFilter.java
+++ Backend/src/main/java/Guthub/Backend/Security/JwtAuthenticationFilter.java
@@ -32,7 +32,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter
                                     @NonNull FilterChain filterChain)
             throws ServletException, IOException
     {
-        if (request.getServletPath().contains("auth/"))
+        if (request.getRequestURI().contains("/auth/"))
         {
             filterChain.doFilter(request, response);
             return;
