c93137864304ec93e36c3121391b177d84725b26
Fabian Mahdi <Fabian@Mahdi.at>
2024-10-06 19:18:40 +0800

    replace usage of 'Stream.collect(Collectors.toList())' with 'Stream.toList()' and ensure that the returned list is unmodified.

--- Backend/src/main/java/guthub/backend/mappers/UserMapper.java
+++ Backend/src/main/java/guthub/backend/mappers/UserMapper.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class UserMapper
@@ -46,6 +45,6 @@ public class UserMapper
     {
         return users.stream()
                 .map(this::toUserDto)
-                .collect(Collectors.toList());
+                .toList();
     }
 }
