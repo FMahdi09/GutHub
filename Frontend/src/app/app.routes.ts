9b072794e607852aa1104c2e632eccaf624e5f61
Fabian Mahdi <Fabian@Mahdi.at>
2024-10-10 14:34:55 +0800

    added explore navbar + dummy list components

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,6 +4,9 @@ import {ExploreComponent} from "./modules/explore/explore.component";
 import {FAQComponent} from "./modules/faq/faq.component";
 import {LoginComponent} from "./modules/login/login.component";
 import {RegisterComponent} from "./modules/register/register.component";
+import {UserListComponent} from "./modules/explore/user-list/user-list.component";
+import {RepositoryListComponent} from "./modules/explore/repository-list/repository-list.component";
+import {OrganizationListComponent} from "./modules/explore/organization-list/organization-list.component";
 
 export const routes: Routes = [
     {
@@ -13,6 +16,26 @@ export const routes: Routes = [
     {
         path: "explore",
         component: ExploreComponent,
+        children:
+            [
+                {
+                    path: "",
+                    redirectTo: "repositories",
+                    pathMatch: "full"
+                },
+                {
+                    path: "users",
+                    component: UserListComponent
+                },
+                {
+                    path: "repositories",
+                    component: RepositoryListComponent
+                },
+                {
+                    path: "organizations",
+                    component: OrganizationListComponent
+                }
+            ]
     },
     {
         path: "faq",
--- Frontend/src/app/modules/explore/explore.component.ts
+++ Frontend/src/app/modules/explore/explore.component.ts
@@ -1,9 +1,13 @@
 import {Component} from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
     selector: 'app-explore',
     standalone: true,
-    imports: [],
+    imports: [
+        RouterLink,
+        RouterLinkActive
+    ],
     templateUrl: './explore.component.html',
     styleUrl: './explore.component.scss'
 })
