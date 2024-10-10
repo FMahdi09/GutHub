38e9bcb556b76e060754b10c76bae39c31c0cfba
Fabian Mahdi <Fabian@Mahdi.at>
2024-10-10 14:55:17 +0800

    added missing router outlet

--- Frontend/src/app/modules/explore/explore.component.ts
+++ Frontend/src/app/modules/explore/explore.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
-import {RouterLink, RouterLinkActive} from "@angular/router";
+import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 
 @Component({
     selector: 'app-explore',
     standalone: true,
     imports: [
         RouterLink,
-        RouterLinkActive
+        RouterLinkActive,
+        RouterOutlet
     ],
     templateUrl: './explore.component.html',
     styleUrl: './explore.component.scss'
