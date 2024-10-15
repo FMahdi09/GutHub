286ffdf9ee27c74dcc9b8a7586285eae99cb012e
Fabian Mahdi <Fabian@Mahdi.at>
2024-10-15 17:02:46 +0800

    added register form skeleton

--- Frontend/src/app/modules/register/register.component.ts
+++ Frontend/src/app/modules/register/register.component.ts
@@ -1,13 +1,30 @@
 import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
     selector: 'app-register',
     standalone: true,
-    imports: [],
+    imports: [
+        ReactiveFormsModule
+    ],
     templateUrl: './register.component.html',
     styleUrl: './register.component.scss'
 })
 export class RegisterComponent
 {
+    registerForm: FormGroup
 
+    constructor()
+    {
+        this.registerForm = new FormGroup({
+            username: new FormControl(""),
+            email: new FormControl(""),
+            password: new FormControl(""),
+            confirmPassword: new FormControl("")
+        });
+    }
+
+    handleSubmit(){
+
+    }
 }
