135dd608c1c709e4d8e4033430a5a31a675757ca
Fabian Mahdi <Fabian@Mahdi.at>
2024-10-16 10:35:43 +0800

    register form: added client side validation

--- Frontend/src/app/modules/register/register.component.ts
+++ Frontend/src/app/modules/register/register.component.ts
@@ -1,11 +1,14 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
     selector: 'app-register',
     standalone: true,
     imports: [
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        NgIf,
+        NgClass
     ],
     templateUrl: './register.component.html',
     styleUrl: './register.component.scss'
@@ -14,17 +17,65 @@ export class RegisterComponent
 {
     registerForm: FormGroup
 
+    get username()
+    {
+        return this.registerForm.get("username")!;
+    }
+
+    get email()
+    {
+        return this.registerForm.get("email")!;
+    }
+
+    get password()
+    {
+        return this.registerForm.get("password")!;
+    }
+
+    get confirmPassword()
+    {
+        return this.registerForm.get("confirmPassword")!;
+    }
+
     constructor()
     {
         this.registerForm = new FormGroup({
-            username: new FormControl(""),
-            email: new FormControl(""),
-            password: new FormControl(""),
-            confirmPassword: new FormControl("")
+            username: new FormControl(
+                "",
+                [
+                    Validators.required,
+                    Validators.minLength(4),
+                    Validators.maxLength(20),
+                ]
+            ),
+            email: new FormControl(
+                "",
+                [
+                    Validators.required,
+                    Validators.email
+                ]
+            ),
+            password: new FormControl(
+                "",
+                [
+                    Validators.required,
+                    Validators.minLength(8),
+                    Validators.maxLength(20),
+                ]
+            ),
+            confirmPassword: new FormControl(
+                "",
+                []
+            ),
         });
     }
 
-    handleSubmit(){
-
+    handleSubmit()
+    {
+        if (!this.registerForm.valid)
+        {
+            console.log("invalid")
+            return;
+        }
     }
 }
